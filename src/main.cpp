78489e255a3ee75c18b6f6d1e36ded4891bbaf30
C4FE1 <heitorcdesousa13@gmail.com>
2024-04-07 18:54:10 -0300

    Removido erro na mensagem de help_page()

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <unistd.h>
 #include <string>
+#include <fstream>
+#include <filesystem>
 #include <cctype>
 #include <map>
 #include "toml.hpp"
 
 
+std::map<std::string, std::string> config;
 std::map<std::string, std::string> packages_by_manager;
 std::map<std::string, std::map<std::string, std::string>> package_manager_commands;
 
@@ -23,12 +26,47 @@ void help_page(){
 	std::cout << "Uso: config-sys pkg-set [opção] {pacotes}" << std::endl;
 	std::cout << "exemplo: config-sys pkg-set load jonhspkgs" << std::endl;
 	std::cout << "opções:" << std::endl;
-	// ToDo
-	//std::cout << "help   => mostra essa mensagem em seu terminal" << std::endl;
+	std::cout << "help   => mostra essa mensagem em seu terminal" << std::endl;
+	//ToDo
 	//std::cout << "list   => lista os conjutos de pacotes" << std::endl;
 	std::cout << "load   => carrega um conjunto de pacotes" << std::endl;
 	std::cout << "unload => descarrega um conjunto de pacotes" << std::endl;
 }
+/*
+bool file_exists(const std::string& file_path) {
+    std::ifstream file(file_path);
+    return file.good();
+}
+
+int get_config_from_file() {
+	std::string user_home = getenv("HOME");
+	std::string user_config = "/.config/pkg-set/config.toml";
+	std::string user_config_path = user_home + user_config; // Fixed the variable name
+	std::string file_path;
+
+	if (file_exists(user_config_path)) {
+        	file_path = user_config_path;
+    	} else {
+        	file_path = "/etc/pkg-set/config.toml";
+    	}
+
+    	toml::table tbl;
+    	try {
+        	tbl = toml::parse_file(file_path);
+        	auto paths = tbl["Paths"].as_table();
+        	for (const auto& path : *paths) {
+            		std::string path_name_by_func = std::string(path.first);
+            		//std::cout << path_name_by_func << ":" << tbl["Paths"][path_name_by_func] << std::endl;
+			config[path_name_by_func] = *tbl["Paths"][path_name_by_func].value<std::string>();
+			std::cout << config[path_name_by_func] << std::endl;
+        	}
+
+    	} catch (const toml::parse_error& err) {
+        	std::cerr << "Failed to read the file: " << err.what() << std::endl; // Changed err to err.what()
+        	return 1;
+    	}
+    	return 0;
+}*/
 
 int get_data_from_file(const std::string& file_path){
 	toml::table tbl;
@@ -75,6 +113,8 @@ int get_data_from_file(const std::string& file_path){
 int main(int argc, char* argv[]){
 	//std::string user_home = getenv( "HOME" );
 	
+	//get_config_from_file();
+
 	if (argc >= 3){
 		get_data_from_file(argv[2]);
 		if(to_lower_func(std::string(argv[1])) == "load"){
